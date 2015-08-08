de811dcd503ed531686f08f840474ce5c1777091
linster <linster247@gmail.com>
2015-08-08 12:21:47 -0600

    Added new model

---
+++ app/src/main/java/ca/stefanm/sayhi/model/MyUserProfile.java
@@ -0,0 +1,9 @@
+package ca.stefanm.sayhi.model;
+
+/**
+ * Created by stefan on 8/8/15.
+ */
+public class MyUserProfile {
+
+    //Contains Nearby Item and Nearby Business Card Item
+}
---
+++ app/src/main/java/ca/stefanm/sayhi/model/NearbyBusinessCardItem.java
@@ -0,0 +1,112 @@
+package ca.stefanm.sayhi.model;
+
+import android.media.Image;
+
+/**
+ * Created by stefan on 8/8/15.
+ */
+public class NearbyBusinessCardItem {
+
+
+    private Integer businesscardid; //Id of this business card on the server
+    private Integer userid;         //Id of the user who belongs to this business card.
+
+    private String FirstName;
+    private String LastName;
+    private String CatchPhrase;
+    private String PhoneNumber;
+    private String CellNumber;
+    private String email;
+    private String website;
+    private String LinkedInURL;
+
+    Image UserPicture;
+
+    public Integer getBusinesscardid() {
+        return businesscardid;
+    }
+
+    public void setBusinesscardid(Integer businesscardid) {
+        this.businesscardid = businesscardid;
+    }
+
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getCatchPhrase() {
+        return CatchPhrase;
+    }
+
+    public void setCatchPhrase(String catchPhrase) {
+        CatchPhrase = catchPhrase;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public String getCellNumber() {
+        return CellNumber;
+    }
+
+    public void setCellNumber(String cellNumber) {
+        CellNumber = cellNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getLinkedInURL() {
+        return LinkedInURL;
+    }
+
+    public void setLinkedInURL(String linkedInURL) {
+        LinkedInURL = linkedInURL;
+    }
+
+    public Image getUserPicture() {
+        return UserPicture;
+    }
+
+    public void setUserPicture(Image userPicture) {
+        UserPicture = userPicture;
+    }
+}
