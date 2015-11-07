c7e971c2799be2bcd8c2e3c2370ee8df8f983ba4
Nathan <eyesniper2@users.noreply.github.com>
2015-11-07 14:01:56 -0700

    Delete MagicAPIKeys.class
    Deleted this old class for security reasons added it to the git ignore is the last push and regenerated keys.

--- app/src/main/java/ca/stefanm/sayhi/services/MagicAPIKeys.java
+++
@@ -1,8 +0,0 @@
-package ca.stefanm.sayhi.services;
-
-/**
- * Created by stefan on 9/27/15.
- */
-public class MagicAPIKeys {
-    public static final String STATIC_MAPS_API_KEY = "AIzaSyBGOfUiqQPgbHBHK73urXn9vL5fH0xzgOA";
-}
